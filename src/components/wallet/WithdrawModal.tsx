183b374f69b033388ca0fff2bc3caa591f364dc8
ethereumtf <myethereumtf@gmail.com>
2025-07-31 11:19:46 +0100

    Bug fix

--- src/components/wallet/WithdrawModal.tsx
+++ src/components/wallet/WithdrawModal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { AlertCircle, ArrowRight } from 'lucide-react'
+import { AlertCircle, ArrowRight, ArrowUpRight } from 'lucide-react'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
