2c53d7d974af72044990f9f2edfe1ac95d90118a
ethereumtf <myethereumtf@gmail.com>
2025-08-02 07:44:15 +0100

    Bug fix

--- src/app/accounts/wallet/page.tsx
+++ src/app/accounts/wallet/page.tsx
@@ -211,7 +211,7 @@ export default function WalletPage() {
 
       <PortfolioHeader 
         totalValue={totalBalance} 
-        isLoading={isLoading}
+        isLoading={loading}
       />
 
       <PrimaryActions
